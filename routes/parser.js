9b5f10eb3d3dc60b9db39597a12b7fad66430223
thanthos <thanthos@yahoo.com>
2015-02-13 16:07:56 +0800

    Parse the People Information currently from Reuters sites. Version 1.0v
    Signed-off-by: thanthos <thanthos@yahoo.com>

---
+++ routes/parser.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+var request = require("request");
+var cheerio = require('cheerio');
+var util    = require('util');
+var JSON    = require('JSON');
+
+
+
+router.get('/', function (req, res, next) {
+
+	var sym = req.query.sym;
+	console.log("sym:"+sym);
+
+	//Tell the request that we want to fetch data, send the results to a callback function
+	request({
+		uri : 'http://www.reuters.com/finance/stocks/companyOfficers?symbol='+sym
+	}, function (err, response, body) {
+		// Hand the HTML response off to Cheerio and assign that to
+		//  a local $ variable to provide familiar jQuery syntax.
+		if (err && response.statusCode !== 200) {
+			console.log('Request error.');
+		}
+		
+		var $ = cheerio.load(body);
+		
+		var dataset = $("#companyNews * .dataTable .dataSmall")
+		var datasetJSON = [];
+		
+		console.log("Data Set Length "+dataset.length);
+		res.setHeader("Content-Type", "application/json; charset=UTF-8 ");
+		
+		for ( var i = 0; i < dataset.length; i++) {
+			console.log("Iteration :"+i);
+			//The following values must co-relate. It should. 
+			var headerArray  = $(dataset[i]).find("tr th").map(function (i, o ) { return $(o).text(); });
+			var dataArray    = $(dataset[i]).find("tr td").map(function (i, o ) { return $(o).text(); });
+			
+			console.log("Number Headers :"+headerArray.length);
+			console.log("Number datafields :"+dataArray.length);
+			
+			var category = [];
+			var offset = 0;
+					
+			for ( var n = 0 ; n < dataArray.length/headerArray.length; n ++ ) {
+				var instance = {};
+				for ( var c = 0; c < headerArray.length; c ++ ) {
+					instance[headerArray[c]] = dataArray[""+(c+offset)];
+				}
+				category.push(instance);
+				offset = offset+headerArray.length;
+			}
+			
+			datasetJSON.push(category);
+		}
+		res.end(JSON.stringify(datasetJSON));
+		
+	});
+});
+module.exports = router;
