76b1067f98f6881cc76ecd54702e37710b1c4ac4
thanthos <thanthos@yahoo.com>
2015-02-23 21:23:32 +0700

    Help with the default app by listing the applications available.
    Signed-off-by: thanthos <thanthos@yahoo.com>

--- apps/index.js
+++ apps/index.js
@@ -7,4 +7,8 @@ var parser = require('./parser');
 router.use('/ssearch', search);
 router.use('/pparser', parser);
 
+router.get('/', function(req,res) {
+	res.render('appIndex',{title:'Application Index',apps:[{"name":"Stock Search","Link":"/apps/ssearch"},{"name":"People Search","Link":"/apps/pparser"}]});
+	});
+
 module.exports = router;
