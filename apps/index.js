a167362da85ef641e7c9b3911a4bd671d089ceb2
thanthos <thanthos@yahoo.com>
2015-03-02 14:11:11 +0700

    Update to SkeletonLayout - This every every jade document Added the new application to the app.jsn and app index page. Added and upate the fragment layout for neo4j graph browsing

--- apps/index.js
+++ apps/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 var search = require('./search');
 var parser = require('./parser');
+var staticBrowser = require('./staticRelationBrowser');
 var appsListing = require('./apps.json');
 
 router.use('/ssearch', search);
 router.use('/pparser', parser);
+router.use('/staticBrowser1', staticBrowser);
 
 router.get('/', function(req,res) {
 	res.render('default_application_main',{title:'ZeusView: Applications Index', "apps":appsListing,"user":req.user});
