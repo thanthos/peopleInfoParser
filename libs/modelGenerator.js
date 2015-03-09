7e8267aa83d9f797542d8b1baa8f458b9a32c5d6
thanthos <thanthos@yahoo.com>
2015-03-09 21:12:45 +0700

    Bug: Fixed the population of the value "Since" Refactor: The generation of the static stock model

--- libs/modelGenerator.js
+++ libs/modelGenerator.js
@@ -11,6 +11,9 @@ var log = bunyan.createLogger({
 		streams : [{
 				stream : process.stdout,
 				level : "debug"
+			}, {
+				path : './logs/modelGenerator.log',
+				level : "debug"
 			}
 		]
 	});
@@ -26,8 +29,8 @@ var ModelGenerator = function () {
 	//Keep tracks of the data in database which are considered duplicates.e
 	/*
 	 *	Duplicate data example:
-	 *   [{id:{name:"someone",age:20},total:2},
-	 *	 {id:{name:"anotherone",age:21},total:2}]
+	 *   [{_id:{name:"someone",age:20},total:2},
+	 *	 {_id:{name:"anotherone",age:21},total:2}]
 	 */
 	this.duplicates = [];
 	this.tobeUpdated = [];
@@ -44,31 +47,40 @@ var ModelGenerator = function () {
 
 	//Process the duplicate staff records. Merge the symbols into the master records. The master records, is define as the one with more information.
 	this.processDuplicates = function () {
-		log.info("Duplicate Found:: Processing %", self.duplicates.length);
 		for (var i in self.duplicates) {
 			Staff.find({
-				"name" : self.duplicates[i].name,
-				"age" : self.duplicates[i].age
+				"name" : self.duplicates[i]._id.name,
+				"age" : self.duplicates[i]._id.age
 			}, function (err, results) {
+
 				if (err) {
-					log.warn("Error looking up %s %s", self.duplicates[i].name, self.duplicates[i].age);
+					log.warn("Error looking up %s %s", self.duplicates[i]._id.name, self.duplicates[i]._id.age);
 				} else {
 					//Merge the duplicate
+					log.debug("Found Results %s", JSON.stringify(results));
 					var masterRec = results[0];
 					for (var x = 1; x < results.length; x++) {
-						if (masterRec.symbols.length < staff[x].symbols.length) { //execute swap
-							staff[x] = [masterRec, masterRec = results[x]][0];
+						if (masterRec.symbols.length < results[x].symbols.length) { //execute swap
+							results[x] = [masterRec, masterRec = results[x]][0];
+							log.debug("Master Record Swapping Position");
 						}
-						masterRec.symbols.push(results[x].symbols);
+						masterRec.symbols.push(results[x].symbols[0]);
 						Staff.remove({
-							"name" : staff[x].name,
-							"age" : staff[x].age,
-							"symbols.symbol" : staff[x].symbols[0].symbol
+							"name" : results[x].name,
+							"age" : results[x].age,
+							"symbols.symbol" : results[x].symbols[0].symbol
+						}, function (e) {
+							if (e) {
+								log.debug("Error Removing Record :%s", e);
+							} else {
+								log.debug("Record Removed");
+							}
 						});
 						Staff.update({
 							"_id" : masterRec._id
 						}, {
-							symbols : masterRec.symbols
+							symbols : masterRec.symbols,
+							updatedOn: new Date()
 						}, function (updateErr, num, raw) {
 							if (updateErr) {
 								log.warn("Error Updating Duplicate Staff");
@@ -77,58 +89,58 @@ var ModelGenerator = function () {
 							}
 						});
 					}
-
 				}
 			});
-		}
+		};
 		//Move on to the next regardless if procesed successfully. Else it will be reflected in the next round of generation.
 		ee.emit("duplicateProcessed");
 	};
+	this.processStockCallBack = function (err, docs) {
+		if (err) {
+			log.error("Error Retrieving Stocks for Population.");
+			ee.emit("generationCompleted");
+		} else {
+			try {
+				for (var i in docs) {
+					var doc = docs[i];
+					var availcolor = ["blue", "green", "black", "darkgreen", "indianred"];
+					var color = availcolor[(Math.random() * 5) | 0]
+						if (doc.symbol == 'SGXL.SI')
+							color = "Yellow";
+						self.graph.nodes.push({
+							"name" : doc.name,
+							"id" : doc.symbol,
+							"size" : 0.8,
+							"label" : doc.name,
+							"x" : 50 * Math.cos(2 * i * Math.PI / docs.length) + 5,
+							"y" : 60 * Math.sin(2 * i * Math.PI / docs.length),
+							"nodeType" : "stock",
+							"color" : color
+						});
+				}
+				log.debug("Loaded %s stocks.", docs.length);
+				ee.emit("populatedNodes");
+			} catch (findError) {
+				log.error("Error Looking up Stocks. %s", findError);
+				ee.emit("generationCompleted");
+			}
+
+		}
+	}
 	this.processStocks = function () {
 		//Populate the stocks portion of the nodes.
-		log.info("Populating Stocks."+arguments.length);
+		log.info("Populating Stocks.");
 		Stock.find({
 			"exchange" : "Stock Exchange of Singapore"
 		}, "symbol name",
-			function (err, docs) {
-			if (err) {
-				log.error("Error Retrieving Stocks for Population.");
-				ee.emit("generationCompleted");
-			} else {
-				try {
-					for (var i in docs) {
-						var doc = docs[i];
-						var availcolor = ["blue", "green", "black", "darkgreen", "indianred"];
-						var color = availcolor[(Math.random() * 5) | 0]
-							if (doc.symbol == 'SGXL.SI')
-								color = "Yellow";
-							self.graph.nodes.push({
-								"name" : doc.name,
-								"id" : doc.symbol,
-								"size" : 0.8,
-								"label" : doc.name,
-								"x" : 50 * Math.cos(2 * i * Math.PI / nodes.length) + 5,
-								"y" : 60 * Math.sin(2 * i * Math.PI / nodes.length),
-								"nodeType" : "stock",
-								"color" : color
-							});
-					}
-					log.debug("Loaded %s stocks.", docs.length);
-					ee.emit("populatedNodes");
-				} catch (findError) {
-					log.error("Error Looking up Stocks %s", findError);
-					ee.emit("generationCompleted");
-				}
-			}
-		});
+			self.processStockCallBack);
 	};
-	this.processStaffs = function () {
-		log.info("Populating Staffs.");
-		Staff.find({}, "name age symbols", function (err, docs) {
-			if (err) {
-				log.error("Error Retriving Staffs");
-				ee.emit("generationCompleted");
-			} else {
+	this.processStaffsCallBack = function (err, docs) {
+		if (err) {
+			log.error("Error Retriving Staffs Collections");
+			ee.emit("generationCompleted");
+		} else {
+			try {
 				for (var i in docs) {
 					var doc = docs[i];
 					var symbols = [];
@@ -136,42 +148,59 @@ var ModelGenerator = function () {
 						log.warn("Record Quality Issue. %s %s", doc.name, doc.age);
 						continue;
 					}
-					for (var x = 0; x < doc.symbols.length; x++) {
-						//only put the symbol info
-						symbols.push(doc.symbols[x].symbol);
+					try {
+						for (var x = 0; x < doc.symbols.length; x++) {
+							//only put the symbol info
+							symbols.push(doc.symbols[x].symbol);
+						}
+					} catch (ee) {
+						log.error("Error Processing Staff Association. %s. Error: %s", doc.name, ee);
+						symbols = [];
 					}
 					self.graph.nodes.push({
 						"name" : doc.name,
 						"age" : doc.age,
 						"id" : i,
-						"size" : 0.35,
+						"size" : 0.15,
 						"label" : doc.name,
-						"symbol" : symbol,
-						"x" : 110 * Math.cos(2 * i * Math.PI / nodes.length),
-						"y" : 120 * Math.sin(2 * i * Math.PI / nodes.length),
+						"symbol" : symbols,
+						"x" : 110 * Math.cos(2 * i * Math.PI / docs.length),
+						"y" : 120 * Math.sin(2 * i * Math.PI / docs.length),
 						"color" : "purple",
 						"nodeType" : "staff"
 					});
 				}
-				log.info("Loaded %s Staff node", docs.length);
-				ee.emit("populatedStaff");
+			} catch (findError) {
+				log.error("Error Prcessing Staff. %s", findError);
+				ee.emit("generationCompleted");
 			}
-		});
+			log.info("Loaded %s Staff node", docs.length);
+			ee.emit("populatedStaff");
+		}
+	}
+	this.processStaffs = function () {
+		log.info("Populating Staffs.");
+		Staff.find({}, "name age symbols", self.processStaffsCallBack);
 	};
 	this.establishEdges = function () {
-		log.info("Linking Nodes. Establishing Edges");
+		log.info("Linking Nodes. Establishing Edges (%s)", self.graph.nodes.length);
 		try {
 			for (var i in self.graph.nodes) {
-				var node = graph.nodes[i];
+				var node = self.graph.nodes[i];
 				log.debug("Node: " + node.id);
 				if (node.nodeType == 'staff') {
 					for (var x = 0; x < node.symbol.length; x++) {
-						self.graph.edges.push({
+						var edge = {
 							"id" : "e" + i + "x" + x,
 							"source" : node.symbol[x],
 							"target" : node.id,
 							"label" : "works for"
-						});
+						};
+						if (node.symbol.length > 1) {
+							edge.type = 'curvedArrow';
+						} //else just leave it as default.
+
+						self.graph.edges.push(edge);
 					}
 				}
 
@@ -185,17 +214,17 @@ var ModelGenerator = function () {
 					}
 				}
 			}
+			log.info("Model Completed. N:%s E:%s", self.graph.nodes.length, self.graph.edges.length);
 			ee.emit("modelCompleted");
-			log.info("Model Completed. N:%s E:%s", graph.nodes.length, graph.edges.length);
 		} catch (e) {
-			log.error("Error: Generic");
+			log.error("Error: Generic %s", e);
 			ee.emit("generationCompleted");
 		}
 	};
 	this.exportToFile = function () {
 		log.info("Model Ready to Export");
 		try {
-			fs.writeFile(self.outputFile, JSON.stringify(self.graphs), function (err) {
+			fs.writeFile(self.outputFile, JSON.stringify(self.graph), function (err) {
 				if (err) {
 					log.error("Error to File: %s", self.outputFile);
 				} else {
@@ -205,54 +234,70 @@ var ModelGenerator = function () {
 		} catch (e) {
 			log.error("Error to File: %s", self.outputFile);
 		}
-		ee.emit("generationCompleted.");
-
+		ee.emit("generationCompleted");
 	};
 	this.generationCompleted = function () {
+		log.info("Generation Completed. Set state to NOT STARTED");
+		try {
+
+			//because this event might not be triggered. All the time. So clean up.
+			if (ee.listeners("duplicateFound").length > 0) {
+				ee.removeAllListeners("duplicateFound")
+			}
+			log.debug("Listeners Length : %s %s %s %s %s %s", ee.listeners("duplicateFound").length,
+				ee.listeners("duplicateProcessed").length,
+				ee.listeners("populatedNodes").length,
+				ee.listeners("populatedStaff").length,
+				ee.listeners("modelCompleted").length,
+				ee.listeners("generationCompleted").length);
+		} catch (e) {
+			log.error("Error Checking on Listner: %s", e);
+		}
 		self.state = "NOT STARTED";
 	};
 
-	ee.on("duplicateFound", this.processDuplicates)
-	.on("duplicateProcessed", this.processStocks)
-	.on("populatedNodes", this.processStaffs)
-	.on("populatedStaff", this.establishEdges)
-	.on("modelCompleted", this.exportToFile)
-	.on("generationCompleted", this.generationCompleted);
-
-	this.callBack = function (err,results){
-		console.log(self);
+	//this are convenience method.
+	this.startCallBack = function (err, results) {
 		if (err) {
-				log.error("Finding Duplicate: DB Aggregate Error");
-				
-				ee.emit("generationCompleted", self);
-			} else {
-				//process results.
-				log.info("Found Possible %s duplicates ", results.length);
-
-				if (results.length == 0) {
-					log.debug("Skipping: Goto Populating");
-					ee.emit("duplicateProcessed", self);
-					
-				} else {
-					for (var i in results) {
-						var res = results[i];
-						log.debug("Duplicate %s : %s", i, results[i]._id);
-						self.duplicates.push(results[i]);
-					}
-					log.debug("Skipping: Got to Populating");
-					ee.emit("duplicateFound", self );
-				}
+			log.error("Finding Duplicate: DB Aggregate Error");
+			ee.emit("generationCompleted", self);
+			//hand over to another method
+		} else {
+			//process results.
+			log.info("Found Possible %s duplicates ", results.length);
+			if (results.length == 0) {
+				log.debug("Skipping: Goto Populating");
+				ee.emit("duplicateProcessed", self);
 				//hand over to another method
-				
+
+			} else {
+				for (var i in results) {
+					var res = results[i];
+					self.duplicates.push(results[i]);
+				}
+				ee.emit("duplicateFound", self);
+				//hand over to another method
 			}
-			log.debug("End Aggregation Function");
+		}
 	}
-	
+
 	this.start = function () {
-		log.debug("Current State %s", this.state);
-		if (self.state != "NOT STARTED")
-			throw "Currently Generating";
-		self.state = "STARTED";
+
+		var arraysOfListenersforGenerator = ee.listeners("generationCompleted");
+		if (arraysOfListenersforGenerator != null) {
+			log.info("Number of Generation Completed Listeners %s", arraysOfListenersforGenerator.length);
+			if (arraysOfListenersforGenerator.length > 0) {
+				throw "Generation Already In Progress. ";
+			}
+		}
+
+		ee.once("duplicateFound", this.processDuplicates)
+		.once("duplicateProcessed", this.processStocks)
+		.once("populatedNodes", this.processStaffs)
+		.once("populatedStaff", this.establishEdges)
+		.once("modelCompleted", this.exportToFile)
+		.once("generationCompleted", this.generationCompleted);
+
 		//Get the list of duplicates.
 		Staff.aggregate()
 		.group({
@@ -269,11 +314,9 @@ var ModelGenerator = function () {
 				$gt : 1
 			}
 		})
-		.exec(this.callBack);
-
-/*		{
-			
-		}); */
+		.exec(this.startCallBack);
+		return;
 	}
 }
+
 module.exports = ModelGenerator;
